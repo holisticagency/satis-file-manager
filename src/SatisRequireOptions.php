458a7a47b808fcfa138773f10088177a70837bda
JamesRezo <james@rezo.net>
2015-04-25 20:22:00 +0200

    Add Require options object

---
+++ src/SatisRequireOptions.php
@@ -0,0 +1,169 @@
+<?php
+
+/**
+ * This file is part of holisatis.
+ *
+ * (c) Gil <gillesodret@users.noreply.github.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace holisticagency\satis\utilities;
+
+use Serializable;
+
+/**
+ * Satis configuration archive options utilities.
+ *
+ * @author James <james@rezo.net>
+ */
+class SatisRequireOptions implements Serializable
+{
+    private $all;
+
+    private $require;
+
+    private $dependencies;
+
+    private $devDependencies;
+
+    private function check()
+    {
+        if (!$this->all && empty($this->require)) {
+            $this->all = true;
+        }
+    }
+
+    private function cleanArray(array $require)
+    {
+        //Unset default values
+        if (isset($require['require-all']) && false === $require['require-all']) {
+            unset($require['require-all']);
+        }
+        if (isset($require['require']) && is_array($require['require']) && empty($require['require'])) {
+            unset($require['require']);
+        }
+        if (isset($require['require-dependencies']) && false === $require['require-dependencies']) {
+            unset($require['require-dependencies']);
+        }
+        if (isset($require['require-dev-dependencies']) && false === $require['require-dev-dependencies']) {
+            unset($require['require-dev-dependencies']);
+        }
+
+        //Unset non-allowed keys
+        foreach ($require as $key => $value) {
+            if (!in_array($key, array(
+                'require-all',
+                'require',
+                'require-dependencies',
+                'require-dev-dependencies'
+            ))) {
+                unset($require[$key]);
+            }
+        }
+
+        return $require;
+    }
+
+    public function __construct()
+    {
+        $this->all = true;
+        $this->require = array();
+        $this->dependencies = false;
+        $this->devDependencies = false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function serialize()
+    {
+        return serialize($this->getOptions());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function unserialize($serialized)
+    {
+        $require = $this->cleanArray(unserialize($serialized));
+
+        $this->all = isset($require['require-all']) && true === $require['require-all'];
+        $this->require = (isset($require['require']) && is_array($require['require'])) ?
+            $require['require'] :
+            array();
+        $this->dependencies = isset($require['require-dependencies']) &&
+            true === $require['require-dependencies'];
+        $this->devDependencies = isset($require['require-dev-dependencies']) &&
+            true === $require['require-dev-dependencies'];
+    }
+
+    public function setAll($all = true)
+    {
+        $this->all = (bool) $all;
+
+        return $this;
+    }
+
+    public function getAll()
+    {
+        return $this->all;
+    }
+
+    public function setRequire($requirePackageName, $requirePackageVersion = '*')
+    {
+        if(is_null($requirePackageVersion)) {
+            if (array_key_exists($requirePackageName, $this->require)) {
+                unset($this->require[$requirePackageName]);
+            }
+
+            return $this;
+        }
+        $this->require = array_merge($this->require, array($requirePackageName => $requirePackageVersion));
+
+        return $this;
+    }
+
+    public function getRequire()
+    {
+        return $this->require;
+    }
+
+    public function setDependencies($dependencies = true)
+    {
+        $this->dependencies = (bool) $dependencies;
+
+        return $this;
+    }
+
+    public function getDependencies()
+    {
+        return $this->dependencies;
+    }
+
+    public function setDevDependencies($devDependencies = true)
+    {
+        $this->devDependencies = (bool) $devDependencies;
+
+        return $this;
+    }
+
+    public function getDevDependencies()
+    {
+        return $this->devDependencies;
+    }
+
+    public function getOptions()
+    {
+        $this->check();
+        $require = array(
+            'require-all' => $this->getAll(),
+            'require' => $this->getRequire(),
+            'require-dependencies' => $this->getDependencies(),
+            'require-dev-dependencies' => $this->getDevDependencies(),
+        );
+
+        return $this->cleanArray($require);
+    }
+}
---
+++ tests/SatisRequireOptionsTest.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * This file is part of holisatis.
+ *
+ * (c) Gil <gillesodret@users.noreply.github.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace holisticagency\satis\Test;
+
+use PHPUnit_Framework_TestCase;
+use holisticagency\satis\utilities\SatisRequireOptions;
+
+/**
+ * Require Options Tests.
+ *
+ * @author James <james@rezo.net>
+ */
+class SatisRequireOptionsTest extends PHPUnit_Framework_TestCase
+{
+    protected $require;
+
+    protected function setUp()
+    {
+        $this->require = new SatisRequireOptions();
+    }
+
+    public function testGetDefaultRequireOptions()
+    {
+        $this->assertTrue($this->require->getAll());
+    }
+
+    public function testSerialization()
+    {
+        $this->require->setAll(false);
+        $this->assertEquals(
+            'C:50:"holisticagency\satis\utilities\SatisRequireOptions":29:{a:1:{s:11:"require-all";b:1;}}',
+            serialize($this->require)
+        );       
+        $this->assertTrue($this->require->getAll());
+
+        $this->assertEquals(
+            unserialize('C:50:"holisticagency\satis\utilities\SatisRequireOptions":29:{a:1:{s:11:"require-all";b:1;}}'),
+            $this->require
+        );
+
+        $this->assertEquals(
+            unserialize('C:50:"holisticagency\satis\utilities\SatisRequireOptions":74:{a:2:{s:11:"require-all";b:1;s:8:"wrongkey";s:22:"doSomethingNotExpected";}}'),
+            $this->require
+        );
+
+        $this->require->setDependencies();
+        $this->require->setDevDependencies();
+        $this->assertEquals(
+            unserialize('C:50:"holisticagency\satis\utilities\SatisRequireOptions":97:{a:3:{s:11:"require-all";b:1;s:20:"require-dependencies";b:1;s:24:"require-dev-dependencies";b:1;}}'),
+            $this->require
+        );
+    }
+
+    public function testBasicSetters()
+    {
+        $this->require->setAll('SomethingNotBoolean');
+        $this->assertTrue($this->require->getAll());
+
+        $this->require->setDependencies(null);
+        $this->assertFalse($this->require->getDependencies());
+    }
+
+    public function testAddRequire()
+    {
+        $this->require->setRequire('vendor/name');
+
+        $require = $this->require->getRequire();
+        $this->assertEquals('*', $require['vendor/name']);
+    }
+
+    /**
+     * @depends testAddRequire
+     */
+    public function testReplaceRequire()
+    {
+        $this->require->setRequire('vendor/name', '1.0');
+
+        $require = $this->require->getRequire();
+        $this->assertEquals('1.0', $require['vendor/name']);
+    }
+
+    public function testRemoveRequire()
+    {
+        $this->require->setRequire('vendor/name');
+        $this->require->setRequire('vendor/name', null);
+
+        $require = $this->require->getRequire();
+        $this->assertEmpty($require);
+    }
+
+    public function testRemoveNotExistingRequire()
+    {
+        $this->require->setRequire('vendor/name', null);
+
+        $require = $this->require->getRequire();
+        $this->assertEmpty($require);
+    }
+
+    public function testRequireAllUnset()
+    {
+        $this->require->setRequire('vendor/name')->setAll(false);
+        $this->assertEquals(
+            'C:50:"holisticagency\satis\utilities\SatisRequireOptions":53:{a:1:{s:7:"require";a:1:{s:11:"vendor/name";s:1:"*";}}}',
+            serialize($this->require)
+        );       
+        $this->assertFalse($this->require->getAll());
+    }
+}
